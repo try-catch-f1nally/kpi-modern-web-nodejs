d534462d7285c5018cda1a8f8c3373e2cf12684a
trycatchfinally <finally.catch.try@gmail.com>
2022-10-29 21:10:00 +0300

    Add eslint

---
+++ .eslintrc.cjs
@@ -0,0 +1,26 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    'plugin:import/recommended',
+    'plugin:import/typescript'
+  ],
+  overrides: [],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    tsconfigRootDir: __dirname,
+    project: ['./tsconfig.json']
+  },
+  plugins: [
+    '@typescript-eslint',
+    'import'
+  ],
+  rules: {}
+};
